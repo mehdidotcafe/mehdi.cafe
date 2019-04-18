22fc84bfdb2cf928371c82e80f972c3f9b4c026b
Mehdi Meddour <meddou.dev@gmail.com>
2019-04-18 05:03:35 +0200

    prod build

---
+++ build/precache-manifest.2c953b7c93e7598c0f2573274120023e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1cdaee2578acd36cc3fc",
+    "url": "/static/js/main.1cdaee25.chunk.js"
+  },
+  {
+    "revision": "0a729bd25eeeabbd4ebb",
+    "url": "/static/js/1.0a729bd2.chunk.js"
+  },
+  {
+    "revision": "1cdaee2578acd36cc3fc",
+    "url": "/static/css/main.8d420983.chunk.css"
+  },
+  {
+    "revision": "0a729bd25eeeabbd4ebb",
+    "url": "/static/css/1.911306b5.chunk.css"
+  },
+  {
+    "revision": "7e90bb41affc60664ee4e788199ab849",
+    "url": "/index.html"
+  }
+];
