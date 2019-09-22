8ce4948265955fab9fefd078a8c78bedac5628f8
Mehdi Meddour <contact@meddou.com>
2019-09-22 17:24:01 +0200

    fix main dark color

---
+++ build/precache-manifest.76af2931d52bf6ef714488f780e0535b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50c5a68f49c38f248cc50fcbf608f452",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3fa5a9dd6c5dc3937a7",
+    "url": "/static/css/2.d819a5bb.chunk.css"
+  },
+  {
+    "revision": "e00c531c729050651ff8",
+    "url": "/static/css/main.b1f4e180.chunk.css"
+  },
+  {
+    "revision": "b3fa5a9dd6c5dc3937a7",
+    "url": "/static/js/2.52e8b752.chunk.js"
+  },
+  {
+    "revision": "e00c531c729050651ff8",
+    "url": "/static/js/main.c2a9bef9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
