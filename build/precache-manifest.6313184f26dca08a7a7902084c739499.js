a971e54b0b64fa19daf5190e17a7f74e91e870c9
Mehdi Meddour <contact@meddou.com>
2020-06-01 15:44:51 +0200

    prod build

---
+++ build/precache-manifest.6313184f26dca08a7a7902084c739499.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5cce15e210992356fcca32c182696a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "248ed021fa598db136ba",
+    "url": "/static/css/2.a641e29a.chunk.css"
+  },
+  {
+    "revision": "8842332cc34841dc02c9",
+    "url": "/static/css/main.30faf566.chunk.css"
+  },
+  {
+    "revision": "248ed021fa598db136ba",
+    "url": "/static/js/2.2fe0c3ff.chunk.js"
+  },
+  {
+    "revision": "8842332cc34841dc02c9",
+    "url": "/static/js/main.a55fd62c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
