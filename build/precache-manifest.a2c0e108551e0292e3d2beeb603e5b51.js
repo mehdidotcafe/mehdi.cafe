b8a8b15b47c74b425d588bbcb6fb4cf99bdd65e8
mehdi <contact@meddou.com>
2020-11-28 22:09:22 +0100

    build

---
+++ build/precache-manifest.a2c0e108551e0292e3d2beeb603e5b51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b004d85166baeaa5da05c7f4024eea65",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79f91d8815958b3de87d",
+    "url": "/static/css/2.8242508b.chunk.css"
+  },
+  {
+    "revision": "db3f8bad119a9811dcf4",
+    "url": "/static/css/main.3fd7a289.chunk.css"
+  },
+  {
+    "revision": "79f91d8815958b3de87d",
+    "url": "/static/js/2.df818931.chunk.js"
+  },
+  {
+    "revision": "db3f8bad119a9811dcf4",
+    "url": "/static/js/main.3107733d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
