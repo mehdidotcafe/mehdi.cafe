3ca43e96ed05099cbc152c04b15016f2acc57c9e
mehdi <contact@meddou.com>
2023-01-13 05:10:10 +0100

    3.0.0-beta.5

--- next.config.js
+++ next.config.js
@@ -4,4 +4,19 @@ module.exports = {
   compiler: {
     styledComponents: true,
   },
+
+  i18n: {
+    locales: ['en', 'fr'],
+    defaultLocale: 'en',
+    domains: [
+      {
+        domain: process.env.DOMAIN_EN,
+        defaultLocale: 'en',
+      },
+      {
+        domain: process.env.DOMAIN_FR,
+        defaultLocale: 'fr',
+      },
+    ],
+  },
 }
