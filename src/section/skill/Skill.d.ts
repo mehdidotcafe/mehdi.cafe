cd870ac46df336f961cec6171664ed099a3a0fa2
Mehdi Meddour <contact@mehdi.cafe>
2023-01-27 10:25:36 +0100

    feat: Rework project skill tab (#40)

--- src/section/skill/Skill.d.ts
+++ src/section/skill/Skill.d.ts
@@ -1,8 +1,11 @@
+type Kind = 'language' | 'framework' | 'database' | 'tool' | 'other'
+
 export type Skill = {
   name: string,
   logo: string,
   experience: number,
   color: string,
   isFilterable?: boolean,
-  isVisible?: boolean
+  isVisible?: boolean,
+  kind: Kind,
 }
